b3b073fd9b0743a3288a32f08efe47e496515f1c
nima <nimaqanbari1999@gmail.com>
2022-03-22 03:09:41 -0700

    create pages (Home) and components (veggie & popular) and .env

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
 
+//pages
+import Home from './pages/Home';
+
 function App() {
   return (
     <div>
-      app
+      <Home />
     </div>
   );
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+//components
+import Veggie from '../components/Veggie'
+import Popular from '../components/Popular'
+
+
+const Home = () => {
+  return (
+    <div>
+        <Veggie />
+        <Popular />
+    </div>
+  )
+}
+
+export default Home
