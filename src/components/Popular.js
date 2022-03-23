c338bfbd62939706747625b1b9b0e6e41a26ed27
nima <nimaqanbari1999@gmail.com>
2022-03-23 02:30:18 -0700

    get data, add splide js , add styles for popular, add localStorage

--- src/components/Popular.js
+++ src/components/Popular.js
@@ -1,20 +1,104 @@
-import React,{ useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 
+//splide
+import "@splidejs/splide/dist/css/splide.min.css";
+import { Splide, SplideSlide } from "@splidejs/react-splide";
 //api
 import { getPopular } from "../services/api";
 
+//styled-components
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  margin: 4rem 0;
+`;
+
+const Card = styled.div`
+  min-height: 11rem;
+  border-radius: 2rem;
+  overflow: hidden;
+  position: relative;
+
+  p {
+    position: absolute;
+    z-index: 10;
+    left: 50%;
+    bottom: 0;
+    transform: translate(-50%, 0%);
+    color: #fff;
+    width: 100%;
+    text-align: center;
+    font-weight: 600;
+    font-size: 1rem;
+    height: 40%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+
+  }
+
+  img {
+    border-radius: 2rem;
+    position: absolute;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+`;
+
+const Gradient = styled.div`
+z-index: 3;
+position: absolute;
+width: 100%;
+height: 100%;
+background: linear-gradient(rgba(0,0,0,0), rgba(0,0,0,.5));
+`
+
 const Popular = () => {
-    const [data, setData] = useState([])
-    console.log(data)
-    useEffect(() => {
-        const fetchApi = async () => {
-            setData(await getPopular())
-        }
-        
-        return fetchApi()
-    }, [])
-    
-  return <div>Popular</div>;
+  const [data, setData] = useState([]);
+  console.log(data);
+
+  useEffect(() => {
+    const fetchApi = async () => {
+      const check = localStorage.getItem("popular")
+
+      if(check){
+        setData(JSON.parse(check))
+      }
+      setData(await getPopular());
+    };
+    fetchApi();
+  }, []);
+
+  return (
+    <div>
+      <Wrapper>
+        <h3>Popular Picks</h3>
+        <Splide 
+        options={{
+           perPage: 4 ,
+           arrows:false,
+           pagination:false,
+           drag: "free",
+           gap: "2.5rem",
+           }}>
+          {data.map((item) => {
+            return (
+              <SplideSlide key={item.recipe.label}>
+                <Card>
+                  <p>{item.recipe.label}</p>
+                  <img src={item.recipe.image} alt={item.recipe.label} />
+                  <Gradient />
+                </Card>
+              </SplideSlide>
+            );
+          })}
+        </Splide>
+      </Wrapper>
+    </div>
+  );
 };
 
 export default Popular;
--- src/services/api.js
+++ src/services/api.js
@@ -1,13 +1,18 @@
 import axios from "axios";
 
+//APP_ID
+const APP_ID = "f3c4d543";
+
 //apikey
-const apiKey = "d3b2ee8d212e4269b095f8b32ae3be3d"
+const APP_Key = "1ad54cafe3269cea23810273038d8111";
+
+//BASE_URL
+const BASE_URL = `https://api.edamam.com/api/recipes/v2?type=public&q=chicken&app_id=${APP_ID}&app_key=${APP_Key}`;
 
 const getPopular = async () => {
-    const api = await axios.get(
-      `https://api.spoonacular.com/recipes/random?apiKey=${apiKey}&number=9`
-    )
-    return api.data
-  };
+  const api = await axios.get(BASE_URL);
+  localStorage.setItem("popular", JSON.stringify(api.data.hits))
+  return api.data.hits;
+};
 
-  export {getPopular}
+export { getPopular };
